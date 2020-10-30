7c222926641cef55f19555a1f3ce20a3d8128e63
Ari Kamat <arijkamat@gmail.com>
2020-10-30 16:34:59 -0400

    Pusher class finally works and has right servo values Red Auton Finished, formatted nicely and implemented 2 wg Blue Auton Class initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BLUENovemberRRAuto.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.teamcode.utils.IMU;
+import org.firstinspires.ftc.teamcode.utils.Intake;
+import org.firstinspires.ftc.teamcode.utils.LaunchMath;
+import org.firstinspires.ftc.teamcode.utils.Outtake;
+import org.firstinspires.ftc.teamcode.utils.RingAmount;
+import org.firstinspires.ftc.teamcode.utils.RingDetector;
+import org.firstinspires.ftc.teamcode.utils.WobbleGoal;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+@Autonomous(name= "BlueAuto",group="auton")
+public class BLUENovemberRRAuto extends LinearOpMode {
+    private Intake intake;
+    private Outtake outtake;
+    private RingDetector detector;
+    private IMU imu;
+    private LaunchMath lm;
+    private DistanceSensor left;
+    private DistanceSensor front;
+    private OpenCvCamera phoneCam;
+    private WobbleGoal wg;
+    private RingAmount.Rings state;
+    private Pose2d startPose;
+    private Vector2d leftVector = new Vector2d(-12,6);
+    
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initialize();
+        waitForStart();
+
+    }
+    public void initialize(){
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/REDOctoberRRAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/REDOctoberRRAuto.java
@@ -5,17 +5,17 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
-import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
-import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.utils.CONFIG;
 import org.firstinspires.ftc.teamcode.utils.IMU;
 import org.firstinspires.ftc.teamcode.utils.Intake;
 import org.firstinspires.ftc.teamcode.utils.LaunchMath;
 import org.firstinspires.ftc.teamcode.utils.Outtake;
+import org.firstinspires.ftc.teamcode.utils.Pusher;
 import org.firstinspires.ftc.teamcode.utils.RRQuickStart.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.utils.RingAmount;
 import org.firstinspires.ftc.teamcode.utils.RingDetector;
@@ -24,7 +24,7 @@ import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
-
+@Autonomous(name = "Red Auto", group="Production")
 public class REDOctoberRRAuto extends LinearOpMode {
     private SampleMecanumDrive drive;
     private Intake intake;
@@ -39,37 +39,27 @@ public class REDOctoberRRAuto extends LinearOpMode {
     private WobbleGoal wg;
     private RingAmount.Rings state;
     private Pose2d startPose;
-
+    private Pusher pusher;
     private Vector2d leftVector = new Vector2d(-12,-6);
     private Vector2d centerVector = new Vector2d(-12,-12);
     private Vector2d rightVector = new Vector2d(-12,-18);
-
+    private Vector2d zone;
+    Trajectory startToPowerShotRight;
+    Trajectory powerShotRightToCenter;
+    Trajectory powerShotCenterToLeft;
+    Trajectory psLeftToWgZone;
+    Trajectory wGZoneToWg2;
+    Trajectory wg2ToZone;
+    Trajectory zone2Line;
     @Override
     public void runOpMode() throws InterruptedException {
         initialize();
-        Trajectory startToPowerShotRight = drive.trajectoryBuilder(startPose)
-                .splineTo(rightVector, Math.toRadians(-90.0))
-                .build();
-        Trajectory powerShotRightToCenter = drive.trajectoryBuilder(startToPowerShotRight.end())
-                .splineTo(centerVector, Math.toRadians(-90.0))
-                .build();
-        Trajectory powerShotCenterToLeft = drive.trajectoryBuilder(powerShotRightToCenter.end())
-                .splineTo(leftVector, Math.toRadians(-90.0))
-                .build();
-        Trajectory powerShotToA = drive.trajectoryBuilder(powerShotCenterToLeft.end())
-                .splineTo(new Vector2d(0,-50), Math.toRadians(180.0))
-                .build();
-        Trajectory powerShotToB = drive.trajectoryBuilder(powerShotCenterToLeft.end())
-                .splineTo(new Vector2d(33,-36), Math.toRadians(180.0))
-                .build();
-        Trajectory powerShotToC = drive.trajectoryBuilder(powerShotCenterToLeft.end())
-                .splineTo(new Vector2d(50,-50), Math.toRadians(180.0))
-                .build();
-        Pose2d endOfABorC;
         state = detector.getState();
-        waitForStart();
+        createTrajs(state);
+        sleep(1000);
         wg.grab();
         wg.lift();
+        waitForStart();
         sleep(500);
         wg.stop();
         while(opModeIsActive()){
@@ -81,40 +71,27 @@ public class REDOctoberRRAuto extends LinearOpMode {
             shootRing();
             drive.followTrajectory(powerShotCenterToLeft);
             shootRing();
-            endOfABorC = powerShotToA.end();
-            switch(state){
-                case ZERO:
-                    //position A
-                    drive.followTrajectory(powerShotToA);
-                    endOfABorC = powerShotToA.end();
-                    break;
-                case ONE:
-                    //position B
-                    drive.followTrajectory(powerShotToB);
-                    endOfABorC = powerShotToB.end();
-                    break;
-                case FOUR:
-                    //position C
-                    drive.followTrajectory(powerShotToC);
-                    endOfABorC = powerShotToC.end();
-                    break;
-            }
+            drive.followTrajectory(psLeftToWgZone);
             wg.release();
-            Trajectory parkOnLine = drive.trajectoryBuilder(endOfABorC)
-                    .splineTo(new Vector2d(0,-24), Math.toRadians(180.0))
-                    .build();
-            drive.followTrajectory(parkOnLine);
+            drive.followTrajectory(wGZoneToWg2);
+            wg.grab();
+            wg.lift();
+            drive.followTrajectory(wg2ToZone);
+            wg.release();
+            drive.followTrajectory(zone2Line);
             break;
         }
     }
     public void shootRing(){
-        //outtake.pushRing();
-        //outtake.pullRing();
+        pusher.push();
+        sleep(500);
+        pusher.pull();
     }
     public void initialize(){
         drive = new SampleMecanumDrive(hardwareMap);
         lm = new LaunchMath(60);
         intake = new Intake(hardwareMap);
+        pusher = new Pusher(hardwareMap);
         outtake = new Outtake(hardwareMap);
         wg = new WobbleGoal(hardwareMap);
         back = hardwareMap.get(DistanceSensor.class, CONFIG.BACKDIST);
@@ -128,7 +105,7 @@ public class REDOctoberRRAuto extends LinearOpMode {
         telemetry.addData("Right Readout",rightD);
         telemetry.addData("Back Readout",backD);
         telemetry.update();
-        startPose = new Pose2d(-72+backD,-72+rightD,Math.toRadians(-90.0));
+        startPose = new Pose2d(-72+backD+CONFIG.centerOfRobit2BackDistance,-72+rightD+CONFIG.centerOfRobit2LeftDistance,Math.toRadians(-90.0));
         drive.setPoseEstimate(startPose);
         telemetry.addLine("Starting camera init");
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
@@ -145,5 +122,53 @@ public class REDOctoberRRAuto extends LinearOpMode {
         telemetry.addLine("Finished Initialization");
         telemetry.update();
     }
-
+    public void createTrajs(RingAmount.Rings state){
+        startToPowerShotRight = drive.trajectoryBuilder(startPose)
+                .splineTo(rightVector, Math.toRadians(-90.0))
+                .build();
+        powerShotRightToCenter = drive.trajectoryBuilder(startToPowerShotRight.end())
+                .splineTo(centerVector, Math.toRadians(-90.0))
+                .build();
+        powerShotCenterToLeft = drive.trajectoryBuilder(powerShotRightToCenter.end())
+                .splineTo(leftVector, Math.toRadians(-90.0))
+                .build();
+        switch(state){
+            case ZERO:
+                //position A
+                zone = new Vector2d(0,-50);
+                psLeftToWgZone = drive.trajectoryBuilder(powerShotCenterToLeft.end())
+                        .splineTo(zone, Math.toRadians(180.0))
+                        .build();
+                break;
+            case ONE:
+                //position
+                zone = new Vector2d(33,-36);
+                psLeftToWgZone = drive.trajectoryBuilder(powerShotCenterToLeft.end())
+                        .splineTo(zone, Math.toRadians(180.0))
+                        .build();
+                break;
+            case FOUR:
+                //position C
+                zone = new Vector2d(50,-50);
+                psLeftToWgZone = drive.trajectoryBuilder(powerShotCenterToLeft.end())
+                        .splineTo(zone, Math.toRadians(180.0))
+                        .build();
+                break;
+            default:
+                zone = new Vector2d(0,-50);
+                psLeftToWgZone = drive.trajectoryBuilder(powerShotCenterToLeft.end())
+                        .splineTo(zone, Math.toRadians(180.0))
+                        .build();
+                break;
+        }
+        wGZoneToWg2 = drive.trajectoryBuilder(psLeftToWgZone.end())
+                .splineTo(new Vector2d(-55,-55), Math.toRadians(-90.0))
+                .build();
+        wg2ToZone  = drive.trajectoryBuilder(wGZoneToWg2.end())
+                .splineTo(zone,Math.toRadians(180.0))
+                .build();
+        zone2Line = drive.trajectoryBuilder(wg2ToZone.end())
+                .splineTo(new Vector2d(12,-24),Math.toRadians(90))
+                .build();
+    }
 }
