e1303a85043c74cf1517d7b72e62e30b5113bca0
Ari Kamat <arijkamat@gmail.com>
2021-05-20 17:23:20 -0400

    Recommit prior commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/StatesAuton/Passive0Rings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/StatesAuton/Passive0Rings.java
@@ -1,4 +1,54 @@
 package org.firstinspires.ftc.teamcode.autonomous.StatesAuton;
 
-public class Passive0Rings {
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.utils.Outtake;
+import org.firstinspires.ftc.teamcode.utils.RRQuickStart.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.utils.WobbleGoal;
+
+public class Passive0Rings extends LinearOpMode {
+    private WobbleGoal wg;
+    private Pose2d startPose = new Pose2d(-60.0,-48.0,0.0);
+    private Vector2d wgAVector = new Vector2d(0,-60);
+    private Vector2d finish = new Vector2d(12,-60);
+    private SampleMecanumDrive drive;
+    private Trajectory startToWGA;
+    private Trajectory wgAToFinish;
+    @Override
+    public void runOpMode(){
+        initialize();
+        wg.grab();
+        sleep(2000);
+        wg.lift();
+        sleep(500);
+        wg.stop();
+        waitForStart();
+        drive.followTrajectory(startToWGA);
+        wg.release();
+        wg.stop();
+        drive.followTrajectory(wgAToFinish);
+
+
+    }
+    public void initialize(){
+        drive = new SampleMecanumDrive(hardwareMap);
+        createTrajectories();
+
+
+    }
+    public void createTrajectories(){
+        drive.setPoseEstimate(startPose);
+        startToWGA = drive.trajectoryBuilder(startPose)
+                .splineTo(wgAVector,Math.toRadians(45))
+                .build();
+        wgAToFinish = drive.trajectoryBuilder(startToWGA.end())
+                .splineTo(finish,Math.toRadians(0))
+                .build();
+        /*startToWGA2 =drive.trajectoryBuilder(startToWGA1.end())
+                .strafeTo(wgAVector2)
+                .build();*/
+    }
 }
