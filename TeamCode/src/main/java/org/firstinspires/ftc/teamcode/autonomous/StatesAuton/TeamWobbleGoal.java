56e699209a09aec18bcff4e8b2dac2cc012c6141
Ari Kamat <arijkamat@gmail.com>
2021-05-27 16:51:16 -0400

    Moved Team WobbleGoal to correct package

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/StatesAuton/TeamWobbleGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/StatesAuton/TeamWobbleGoal.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.autonomous;
+package org.firstinspires.ftc.teamcode.autonomous.StatesAuton;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
